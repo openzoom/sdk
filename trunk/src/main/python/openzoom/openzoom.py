c92e919ebb655d27b755886e8d85e4e3d272f6f7
Daniel Gasienica <daniel@gasienica.ch>
2009-03-22 21:44:42 +0000

    Created OpenZoom Tools ImageCreator based on Deep Zoom Tools ImageCreator. Adding support for image sources. Removed dependency on ElementTree library.

--- trunk/src/main/python/openzoom/openzoom.py
+++ trunk/src/main/python/openzoom/openzoom.py
@@ -19,20 +19,12 @@
 #
 ################################################################################
 
-import elementtree.ElementTree as ET
 import deepzoom
 import math
 import os.path
+import xml.dom.minidom
 
-OPENZOOM_NAMESPACE = "http://ns.openzoom.org/openzoom/2008"
-
-# Python 3 compatible format string
-
-#DESCRIPTOR_TEMPLATE_HEADER = """\
-#<?xml version="1.0" encoding="UTF-8"?>
-#<image xmlns="http://ns.openzoom.org/openzoom/2008">
-#    <pyramid width="{width}" height="{height}" tileWidth="{tile_width}" tileHeight="{tile_height}" tileOverlap="{tile_overlap}" type="{type}" origin="{origin}">\
-#"""
+NS_OPENZOOM = "http://ns.openzoom.org/openzoom/2008"
 
 DESCRIPTOR_TEMPLATE_HEADER = """\
 <?xml version="1.0" encoding="UTF-8"?>
@@ -57,13 +49,17 @@ DESCRIPTOR_TEMPLATE_FOOTER = """\
 </image>\
 """
 
-mime_type_map = { "jpg": "image/jpeg",
-                  "png": "image/png" }
+mime_type_map = {"jpg": "image/jpeg",
+                 "png": "image/png"}
 
 
 class OpenZoomDescriptor(object):
     def __init__(self):
         self.uris = []
+        self.sources = []
+    
+    def create(self, source, destination):
+        pass
     
     def open(self, source, type="openzoom"):
         if type == "dzi":
@@ -76,32 +72,71 @@ class OpenZoomDescriptor(object):
         file = open(destination, "w+")
         file.write(self.get_xml())
         file.close()
+    
+    def add_source(self, dimension, format="jpg"):
+        self.sources.append({dimension: dimension, format: format})
         
     def add_uri(self, uri):
         self.uris.append(uri)
     
     def get_xml(self):
         dzi = self.provider
-        out = ""
-        out += DESCRIPTOR_TEMPLATE_HEADER%{"width": self.width,
-                                           "height": self.height,
-                                           "tile_width": self.tile_width,
-                                           "tile_height": self.tile_height,
-                                           "tile_overlap": self.tile_overlap,
-                                           "type": self.type,
-                                           "origin": self.origin} + "\n"
+        doc = xml.dom.minidom.Document()
+        
+        image = doc.createElementNS(NS_OPENZOOM, "image")
+        image.setAttribute("xmlns", NS_OPENZOOM)
+        
+        pyramid = doc.createElementNS(NS_OPENZOOM, "pyramid")
+        pyramid.setAttribute("width", str(self.width))
+        pyramid.setAttribute("height", str(self.height))
+        pyramid.setAttribute("tileWidth", str(self.tile_width))
+        pyramid.setAttribute("tileHeight", str(self.tile_height))
+        pyramid.setAttribute("tileOverlap", str(self.tile_overlap))
+        pyramid.setAttribute("type", str(self.type))
+        pyramid.setAttribute("origin", str(self.origin))
+        
+#        out = ""
+#        out += DESCRIPTOR_TEMPLATE_HEADER%{"width": self.width,
+#                                           "height": self.height,
+#                                           "tile_width": self.tile_width,
+#                                           "tile_height": self.tile_height,
+#                                           "tile_overlap": self.tile_overlap,
+#                                           "type": self.type,
+#                                           "origin": self.origin} + "\n"
         for level in xrange(dzi.num_levels):
             width, height = dzi.get_dimensions(level)
             columns, rows = dzi.get_num_tiles(level)
-            out += LEVEL_TEMPLATE_HEADER%{"width": width, "height": height,
-                                          "columns": columns, "rows": rows} + "\n"
-            for uri in self.uris:
-                file_name = os.path.splitext(os.path.basename(self.source))[0]
-                uri += "/" + file_name + "_files/%(level)s/{column}_{row}.jpg"%{"level": level}
-                out += URI_TEMPLATE%{"uri_template": uri} + "\n"
-            out += LEVEL_TEMPLATE_FOOTER + "\n"
-        out += DESCRIPTOR_TEMPLATE_FOOTER
-        return out
+            level_node = doc.createElementNS(NS_OPENZOOM, "level")
+            level_node.setAttribute("width", str(width))
+            level_node.setAttribute("height", str(height))
+            level_node.setAttribute("columns", str(columns))
+            level_node.setAttribute("rows", str(rows))
+#            out += LEVEL_TEMPLATE_HEADER%{"width": width, "height": height,
+#                                          "columns": columns, "rows": rows} + "\n"
+                                          
+            file_name = os.path.splitext(os.path.basename(self.source))[0]
+            if not self.uris:
+                uri = file_name + "_files/%(level)s/{column}_{row}.jpg"%{"level": level}
+                uri_node = doc.createElementNS(NS_OPENZOOM, "uri")
+                uri_node.setAttribute("template", uri)
+#                out += URI_TEMPLATE%{"uri_template": uri} + "\n"
+                level_node.appendChild(uri_node)
+            else:
+                for uri in self.uris:
+                    uri += "/" + file_name + "_files/%(level)s/{column}_{row}.jpg"%{"level": level}
+                    uri_node = doc.createElementNS(NS_OPENZOOM, "uri")
+                    uri_node.setAttribute("template", uri)
+#                    out += URI_TEMPLATE%{"uri_template": uri} + "\n"
+                    level_node.appendChild(uri_node)
+#            out += LEVEL_TEMPLATE_FOOTER + "\n"
+            pyramid.appendChild(level_node)
+#        out += DESCRIPTOR_TEMPLATE_FOOTER
+        image.appendChild(pyramid)
+        doc.appendChild(image)
+#        return out
+        descriptor = doc.toprettyxml(indent="  ", encoding="UTF-8")
+#        descriptor = doc.toxml(encoding="UTF-8")
+        return descriptor
        
     @property
     def width(self):
@@ -130,3 +165,21 @@ class OpenZoomDescriptor(object):
     @property
     def origin(self):
         return "topLeft"
+
+
+class ImageCreator(object):
+    """Creates OpenZoom images."""
+    def __init__(self, tile_size=256, tile_overlap=1, tile_format="jpg",
+                 image_quality=0.95, resize_filter=None):
+        self.creator = deepzoom.ImageCreator(tile_size, tile_overlap, tile_format, image_quality, resize_filter)
+
+    def create(self, source, destination, sources=[]):
+        """Creates OpenZoom image from a source file and saves it to destination."""
+        self.basename = os.path.basename(source)
+        dzi = destination + "/image.dzi"
+        self.creator.create(source, dzi)
+        self.descriptor = OpenZoomDescriptor()
+        self.descriptor.open(dzi , "dzi")
+        for source in sources:
+            self.descriptor.add_source(source)
+        self.descriptor.save(destination + "/image.xml")
