d2c5ebd55a73dad9705ae578383db87ff9bf3f4b
Daniel Gasienica <daniel@gasienica.ch>
2009-04-07 19:39:59 +0000

    Totally overhauled OpenZoom Endo project. OpenZoom Endo consists basically of a jQuery plugin and a runner script. During the build process both are minified and combined into openzoom-min.js. Added YUICompressor Java library for JavaScript minification.

---
+++ trunk/projects/endo/trunk/src/openzoom.js
@@ -0,0 +1,11 @@
+/*
+ * OpenZoom Endo 0.3
+ * http://openzoom.org/
+ *
+ * Copyright (c) 2007-2009, Daniel Gasienica <daniel@gasienica.ch>
+ * License: GNU General Public License v3
+ * <http://www.gnu.org/licenses/gpl.html>
+ */
+jQuery(document).ready(function() {
+    jQuery.openzoom.run();
+})
