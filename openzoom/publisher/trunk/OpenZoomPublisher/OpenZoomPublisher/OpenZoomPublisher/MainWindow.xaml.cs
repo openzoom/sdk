9fd8b42719969056e0eb56d0bbdd8e6248655994
Daniel Gasienica <daniel@gasienica.ch>
2009-04-17 23:59:09 +0000

    Implemented basic behavior in C#. Moved "Add Images..." button to top. Included Deep Zoom Composer Microsoft DeepZoomTools.dll.

--- openzoom/publisher/trunk/OpenZoomPublisher/OpenZoomPublisher/OpenZoomPublisher/MainWindow.xaml.cs
+++ openzoom/publisher/trunk/OpenZoomPublisher/OpenZoomPublisher/OpenZoomPublisher/MainWindow.xaml.cs
@@ -8,48 +8,102 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Navigation;
 using System.Windows.Forms;
+using Microsoft.DeepZoomTools;
+using System.Collections.ObjectModel;
 
 namespace OpenZoom.Publisher
 {
 	public partial class MainWindow
 	{
+        private const int DEFAULT_TILE_SIZE = 254;
+        private const int DEFAULT_TILE_OVERLAP = 1;
+
+        private ImageCreator imageCreator;
+        private ObservableCollection<Image> images;
+        private String outputFolderPath;
+
         public MainWindow()
 		{
 			this.InitializeComponent();
 			
 			// Insert code required on object creation below this point.
+            imageCreator = new ImageCreator();
+            imageCreator.TileSize = DEFAULT_TILE_SIZE;
+            imageCreator.TileOverlap = DEFAULT_TILE_OVERLAP;
 		}
 
         private void addImage_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Multiselect = true;
-            openFileDialog.Filter = "Image Files (*.jpg, *.png)|*.jpg;*.jpeg;*.png";
+            openFileDialog.Filter = "Images (*.jpg, *.png)|*.jpg;*.jpeg;*.png";
+            images = new ObservableCollection<Image>();
 
             if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 foreach (String file in openFileDialog.FileNames)
                 {
-                    //imageListBox.Items.Add(Path.GetFileName(file));
-                    imageListBox.Items.Add(file);
+                    Image image = new Image(file);
+                    images.Add(image);
                 }
             }
+
+            imageListBox.ItemsSource = images;
         }
 
         private void export_Click(object sender, RoutedEventArgs e)
         {
+            if (!Directory.Exists(outputFolderPath))
+            {
+                System.Windows.Forms.MessageBox.Show("Output path not set!! ;)");
+                return;
+            }
 
+            imageCreator.ImageQuality = qualitySlider.Value / 100.0;
+
+            foreach (Image image in images)
+            {
+                String imageFileName = Path.GetFileName(image.Path);
+                String imageBaseName = Path.GetFileNameWithoutExtension(imageFileName);
+                String imageExtension = Path.GetExtension(image.Path);
+                String imageDirectory = Path.Combine(Path.GetFullPath(outputFolderTextBox.Text), imageBaseName);
+                Directory.CreateDirectory(imageDirectory);
+
+                if (exportOriginalCheckBox.IsChecked == true)
+                    File.Copy(image.Path, Path.Combine(imageDirectory, imageFileName));
+
+                if (imageExtension == ".png")
+                    imageCreator.TileFormat = ImageFormat.Png;
+                else
+                    imageCreator.TileFormat = ImageFormat.Jpg;
+
+                imageCreator.Create(image.Path, Path.Combine(imageDirectory, "image.dzi"));
+            }
         }
 
         private void browseOutputFolder_Click(object sender, RoutedEventArgs e)
         {
             FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
 
-
             if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                outputFolderTextBox.Text = folderBrowserDialog.SelectedPath;
+                outputFolderPath = folderBrowserDialog.SelectedPath;
+                outputFolderTextBox.Text = outputFolderPath;
             }
         }
 	}
+
+    public class Image
+    {
+        public Image(String path)
+        {
+            this.Path = path;
+        }
+
+        public String Path
+        {
+            get;
+            set;
+        }
+    }
 }
