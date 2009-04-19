153b842e2de89afeb20d2cef9835189e3abc40e7
Daniel Gasienica <daniel@gasienica.ch>
2009-04-19 23:09:51 +0000

    Completely overhauled form management using data contexts, data binding and converters. Implemented basic image export using a first resize constraint. Grouped all C# data binding value converters in one file.

--- openzoom/publisher/trunk/OpenZoom/Publisher/MainWindow.xaml.cs
+++ openzoom/publisher/trunk/OpenZoom/Publisher/MainWindow.xaml.cs
@@ -7,31 +7,12 @@ using System.Windows.Forms;
 using System.Xml.Linq;
 using Microsoft.DeepZoomTools;
 using System.Drawing;
+using System.ComponentModel;
+using System.Drawing.Drawing2D;
 
 namespace OpenZoom.Publisher
 {
-    public class Image
-    {
-        public Image(String path)
-        {
-            this.Path = path;
-        }
-
-        public String Path
-        {
-            get;
-            set;
-        }
-    }
-
-    public enum FileFormat
-    {
-        Auto,
-        JPEG,
-        PNG
-    }
-
-	public partial class MainWindow
+    public partial class MainWindow
     {
         private const int DEFAULT_TILE_SIZE = 254;
         private const int DEFAULT_TILE_OVERLAP = 1;
@@ -40,13 +21,18 @@ namespace OpenZoom.Publisher
         private XNamespace openzoomNS;
 
         private ImageCreator imageCreator;
-        private ObservableCollection<Image> images;
-        private String outputFolderPath;
+        private ImagesFormData imagesFormData;
+        private GeneralFormData generalFormData;
+        private SourceFormData sourceFormData;
+        private PyramidFormData pyramidFormData;
+
+        private const String applicationDirectory = "OpenZoom";
+        private String applicationDirectoryPath;
 
         public MainWindow()
-		{
-			this.InitializeComponent();
-			
+        {
+            this.InitializeComponent();
+
             deepzoomNS = "http://schemas.microsoft.com/deepzoom/2008";
             openzoomNS = "http://ns.openzoom.org/2008";
 
@@ -54,12 +40,24 @@ namespace OpenZoom.Publisher
             imageCreator.TileSize = DEFAULT_TILE_SIZE;
             imageCreator.TileOverlap = DEFAULT_TILE_OVERLAP;
 
-            images = new ObservableCollection<Image>();
-		}
+            imagesFormData = (ImagesFormData)(Resources["imagesFormData"]);
+            generalFormData = (GeneralFormData)(Resources["generalFormData"]);
+            sourceFormData = (SourceFormData)(Resources["sourceFormData"]);
+            pyramidFormData = (PyramidFormData)(Resources["pyramidFormData"]);
+
+            // Create app data directory
+            applicationDirectoryPath = Path.Combine(System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), applicationDirectory);
+
+            if (!Directory.Exists(applicationDirectoryPath))
+                Directory.CreateDirectory(applicationDirectoryPath);
+
+            generalFormData.OutputFolderPath = applicationDirectoryPath;
+        }
 
         private void addImagesButton_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
             openFileDialog.Multiselect = true;
             openFileDialog.Filter = "Image Files (*.jpg, *.png)|*.jpg;*.jpeg;*.png";
 
@@ -68,37 +66,32 @@ namespace OpenZoom.Publisher
                 foreach (String file in openFileDialog.FileNames)
                 {
                     Image image = new Image(file);
-                    images.Add(image);
+                    imagesFormData.Images.Add(image);
                 }
             }
-
-            imageListBox.ItemsSource = images;
-            clearButton.Visibility = Visibility.Visible;
         }
 
         private void exportButton_Click(object sender, RoutedEventArgs e)
         {
-            if (!Directory.Exists(outputFolderPath))
+            // Bad karma
+            if (!Directory.Exists(generalFormData.OutputFolderPath))
             {
-                System.Windows.Forms.MessageBox.Show("Output path not set!! ;)");
+                System.Windows.Forms.MessageBox.Show("Output path does not exist.");
                 return;
             }
 
             imageCreator.ImageQuality = qualitySlider.Value / 100.0;
 
-            foreach (Image image in images)
+            foreach (Image image in imagesFormData.Images)
             {
                 String imageFileName = Path.GetFileName(image.Path);
                 String imageBaseName = Path.GetFileNameWithoutExtension(imageFileName);
                 String imageExtension = Path.GetExtension(image.Path);
-                String imageDirectory = Path.Combine(Path.GetFullPath(outputFolderTextBox.Text), imageBaseName);
+                String imageDirectory = Path.Combine(Path.GetFullPath(generalFormData.OutputFolderPath), imageBaseName);
                 Directory.CreateDirectory(imageDirectory);
 
-                if (exportOriginalCheckBox.IsChecked == true)
-                    File.Copy(image.Path, Path.Combine(imageDirectory, imageFileName), true);
-
                 ComboBoxItem selectedItem = fileFormatComboBox.SelectedItem as ComboBoxItem;
-                
+
                 if (selectedItem != null)
                 {
                     String fileFormat = selectedItem.Content.ToString();
@@ -120,40 +113,277 @@ namespace OpenZoom.Publisher
                 String dziPath = Path.Combine(imageDirectory, "image.dzi");
                 imageCreator.Create(image.Path, dziPath);
 
-                System.Drawing.Image bitmap = System.Drawing.Image.FromFile(image.Path);
-                int width = bitmap.Width;
-                int height = bitmap.Height;
+                using (System.Drawing.Image bitmap = System.Drawing.Image.FromFile(image.Path))
+                {
+                    XElement dzi = XElement.Load(dziPath);
+                    dzi.Add(new XAttribute(XNamespace.Xmlns + "openzoom", openzoomNS.NamespaceName));
 
-                XElement dzi = XElement.Load(dziPath);
-                dzi.Add(new XAttribute(XNamespace.Xmlns + "openzoom", openzoomNS.NamespaceName));
-                dzi.Add(new XElement(openzoomNS + "source",
-                        new XAttribute("uri", imageFileName),
-                        new XAttribute("type", imageCreator.TileFormat == ImageFormat.Jpg ? "image/jpeg" : "image/png"),
-                        new XAttribute("width", width),
-                        new XAttribute("height", height)));
-                dzi.Save(dziPath, SaveOptions.DisableFormatting);
+                    if (exportOriginalCheckBox.IsChecked == true)
+                    {
+                        File.Copy(image.Path, Path.Combine(imageDirectory, imageFileName), true);
+
+                        ImageSource imageSource = new ImageSource();
+                        imageSource.Uri = imageFileName;
+                        imageSource.Width = bitmap.Width;
+                        imageSource.Height = bitmap.Height;
+                        imageSource.Format = imageExtension == ".png" ?
+                                                System.Drawing.Imaging.ImageFormat.Png :
+                                                System.Drawing.Imaging.ImageFormat.Jpeg;
+                        addSourceElement(dzi, imageSource);
+                    }
+
+                    sourceFormData.ResizeContraint = (ResizeContraint)resizeConstraintComboBox.SelectedIndex;
+
+                    switch (sourceFormData.ResizeContraint)
+                    {
+                        case ResizeContraint.LongEdge:
+                            {
+                                double aspectRatio = bitmap.Width / (double)bitmap.Height;
+
+                                ImageSource imageSource = new ImageSource();
+
+                                if (aspectRatio > 1.0)
+                                {
+                                    imageSource.Width = sourceFormData.ResizeValue;
+                                    imageSource.Height = (int)(sourceFormData.ResizeValue / aspectRatio);
+                                }
+                                else
+                                {
+                                    imageSource.Width = (int)(sourceFormData.ResizeValue * aspectRatio);
+                                    imageSource.Height = sourceFormData.ResizeValue;
+                                }
+
+                                imageSource.Format = imageExtension == ".png" ?
+                                                        System.Drawing.Imaging.ImageFormat.Png :
+                                                        System.Drawing.Imaging.ImageFormat.Jpeg;
+                                imageSource.Uri = imageBaseName + "-" + imageSource.Width + "x" + imageSource.Height + imageExtension;
+
+                                using (Bitmap output = new Bitmap(imageSource.Width, imageSource.Height))
+                                {
+                                    Graphics g = Graphics.FromImage((System.Drawing.Image)output);
+                                    g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+                                    g.DrawImage(bitmap, 0, 0, imageSource.Width, imageSource.Height);
+                                    g.Dispose();
+
+                                    output.Save(Path.Combine(imageDirectory, imageSource.Uri));
+                                }
+
+                                addSourceElement(dzi, imageSource);
+                                break;
+                            }
+                    }
+
+                    dzi.Save(dziPath, SaveOptions.None);
+                }
             }
 
-            System.Diagnostics.Process.Start("explorer.exe", outputFolderPath);
+            if (afterExportComboBox.SelectedIndex == 1)
+                System.Diagnostics.Process.Start("explorer.exe", generalFormData.OutputFolderPath);
+        }
+
+        private void addSourceElement(XElement element, ImageSource imageSource)
+        {
+            element.Add(new XElement(openzoomNS + "source",
+                        new XAttribute("uri", imageSource.Uri),
+                        new XAttribute("width", imageSource.Width),
+                        new XAttribute("height", imageSource.Height),
+                        new XAttribute("type", imageSource.Type)));
         }
 
         private void browseOutputFolderButton_Click(object sender, RoutedEventArgs e)
         {
             FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.SelectedPath = generalFormData.OutputFolderPath;
 
             if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                outputFolderPath = folderBrowserDialog.SelectedPath;
-                outputFolderTextBox.Text = outputFolderPath;
-            }
+                generalFormData.OutputFolderPath = folderBrowserDialog.SelectedPath;
         }
 
         private void clearButton_Click(object sender, RoutedEventArgs e)
         {
-            if (images != null)
-                images.Clear();
-
-            clearButton.Visibility = Visibility.Hidden;
+            if (imagesFormData.Images != null)
+                imagesFormData.Images.Clear();
         }
-	}
+    }
+
+    public class ImagesFormData
+    {
+        private ObservableCollection<Image> images = new ObservableCollection<Image>();
+
+        public ObservableCollection<Image> Images
+        {
+            get { return images; }
+            set { images = value; }
+        }
+    }
+
+    public class GeneralFormData : INotifyPropertyChanged
+    {
+        public GeneralFormData()
+        {
+            outputFolderPath = "";
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        private String outputFolderPath;
+
+        public String OutputFolderPath
+        {
+            get { return outputFolderPath; }
+            set
+            {
+                outputFolderPath = value;
+                OnPropertyChanged("OutputFolderPath");
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+    public class SourceFormData
+    {
+        public ResizeContraint ResizeContraint
+        {
+            get;
+            set;
+        }
+
+        private int resizeValue = 500;
+
+        public int ResizeValue
+        {
+            get { return resizeValue; }
+            set { resizeValue = value; }
+        }
+
+        public Boolean DontEnlarge
+        {
+            get;
+            set;
+        }
+
+        public Boolean ExportOriginal
+        {
+            get;
+            set;
+        }
+    }
+
+    public class PyramidFormData
+    {
+        public FileFormat FileFormat
+        {
+            get;
+            set;
+        }
+
+        public Double Quality
+        {
+            get;
+            set;
+        }
+    }
+
+    public class AfterExportFormData
+    {
+        public ExportAction ExportAction
+        {
+            get;
+            set;
+        }
+    }
+
+    public class Image
+    {
+        public Image(String path)
+        {
+            Path = path;
+        }
+
+        public String Path
+        {
+            get;
+            set;
+        }
+
+        public ImageSource[] Sources
+        {
+            get;
+            set;
+        }
+    }
+
+    public class ImageSource
+    {
+        public int Width
+        {
+            get;
+            set;
+        }
+
+        public int Height
+        {
+            get;
+            set;
+        }
+
+        public System.Drawing.Imaging.ImageFormat Format
+        {
+            get;
+            set;
+        }
+
+        public String Type
+        {
+            get
+            {
+                if (Format == System.Drawing.Imaging.ImageFormat.Jpeg)
+                    return "image/jpeg";
+
+                if (Format == System.Drawing.Imaging.ImageFormat.Png)
+                    return "image/png";
+
+                throw new NotSupportedException("ImageSource.Type");
+            }
+        }
+
+        public String Uri
+        {
+            get;
+            set;
+        }
+    }
+
+    public enum FileFormat
+    {
+        Auto,
+        JPEG,
+        PNG
+    }
+
+    public enum ExportAction
+    {
+        DoNothing,
+        ShowInExplorer
+    }
+
+    public enum ResizeContraint
+    {
+        LongEdge,
+        ShortEdge,
+        Width,
+        Height
+    }
 }
--- openzoom/publisher/trunk/OpenZoom/Publisher/ValueConverters.cs
+++ openzoom/publisher/trunk/OpenZoom/Publisher/ValueConverters.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Data;
 using System.Globalization;
+using System.Windows;
 
 namespace OpenZoom.Publisher
 {
@@ -20,6 +21,22 @@ namespace OpenZoom.Publisher
                                   object parameter, CultureInfo culture)
         {
             return System.Convert.ToDouble(value);
+        }
+    }
+
+    [ValueConversion(typeof(int), typeof(Visibility))]
+    public class ThresholdToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+                              object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToInt32(value) > 0 ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+                                  object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("ThresholdToVisibilityConverter.ConvertBack");
         }
     }
 }
